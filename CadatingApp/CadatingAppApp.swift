adde768ea06080130934816417ff31ad961792db
Daniel Jeong <89224086+danieljeong24@users.noreply.github.com>
2023-11-27 17:14:46 -0700

    Initial Commit

---
+++ CadatingApp/CadatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadatingAppApp.swift
+//  CadatingApp
+//
+//  Created by Daniel Jeong on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct CadatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
